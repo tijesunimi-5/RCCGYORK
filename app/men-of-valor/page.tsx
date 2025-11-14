47ea0d4510702d561be54e0aa97be4b8b6317460
tijesunimi-5 <tijesunimiidowu16@gmail.com>
2025-11-14 09:52:50 +0100

    added more info to men

--- app/men-of-valor/page.tsx
+++ app/men-of-valor/page.tsx
@@ -6,6 +6,10 @@ import React, { useState } from "react";
 const page = () => {
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
 
+  const menHead = [
+    { id: 1, image: "/ushers/dol.jpg", name: "Jean Dol" }
+  ]
+
   const handleImageClick = (imageUrl: string) => {
     setSelectedImage(imageUrl);
   };
@@ -13,7 +17,12 @@ const page = () => {
   const handleClosePreview = () => {
     setSelectedImage(null);
   };
-  const menPic = [{ id: 1, image: "/men/men2.jpg", text: "first image" }];
+
+  const menPic = [
+    { id: 1, image: "/men/men2.jpg", text: "first image" },
+    { id: 2, image: "/men/men3.jpg", text: "second image" },
+    { id: 3, image: "/men/men.jpg", text: "third image"}
+  ];
 
   return (
     <div className="w-full flex flex-col items-center justify-center mb-20">
@@ -28,13 +37,39 @@ const page = () => {
         />
       </div>
 
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative flex flex-col items-center">
         <h1 className="text-4xl md:text-6xl mb-6 bg-linear-to-r from-gray-900 to-gray-700 bg-clip-text text-transparent text-center mt-10">
           Step into Men of Valor
         </h1>
         <p className="text-gray-700 max-w-3xl mx-auto leading-relaxed text-balance">
           The Men's Ministry at RCCG Living Spring York is a place where men and
-          young men unite to grow in faith, leadership, and service. We are a
+          young men unite to grow in faith, leadership, and service.
+        </p>
+
+        {menHead.map((men) => (
+          <div key={men.id} className='bg-card text-card-foreground flex flex-col rounded-xl overflow-hidden bg-gray-100 shadow-lg hover:shadow-2xl hover:translate-y-2 transition-all border-0 w-full max-w-xs sm:max-w-sm md:max-w-md my-10 '>
+            <div
+              // Parent container remains relative and maintains fixed height
+              className='w-full h-[500px] cursor-pointer relative'
+              onClick={() => handleImageClick(men.image)}
+            >
+              {/* Image sizing: uses fill to perfectly cover the parent container */}
+              <Image
+                src={men.image}
+                alt={men.name}
+                fill
+                className='object-cover'
+              />
+            </div>
+            <div className="content px-4 pb-4 bg-white bg-opacity-50 text-start">
+              <h1 className='font-semibold text-xl'>{men.name}</h1>
+
+            </div>
+          </div>
+        ))}
+
+        <p className="text-gray-700 max-w-3xl mx-auto leading-relaxed text-balance">
+          We are a
           brotherhood committed to fostering spiritual development, authentic
           fellowship, and impactful outreach, empowering men to lead boldly in
           their homes, church, and communities. Through mentorship,
