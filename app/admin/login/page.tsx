e5ea3ad479363f81f8c04c5852d735337ce5eb32
tijesunimi-5 <tijesunimiidowu16@gmail.com>
2025-11-19 06:15:55 +0100

    changed route for login

--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -17,7 +17,7 @@ const LoginPage: React.FC = () => {
     setIsLoading(true);
 
     try {
-      const response = await fetch('/api/admin/login', {
+      const response = await fetch('/api/admin/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
