3e01356fd43b900e5f736f2c850936168aea0b7a
tijesunimi-5 <tijesunimiidowu16@gmail.com>
2025-11-19 01:02:23 +0100

    added features to the rest of pages

--- lib/pg.ts
+++ lib/pg.ts
@@ -2,7 +2,7 @@ import { Pool} from "pg"
 
 if (!process.env.DATABASE_URL) {
   // It's crucial to check for the environment variable before connecting
-  throw new Error("PG_CONNECTION_STRING is not set in environment variables");
+  throw new Error("DATABASE_URL is not set in environment variables");
 }
 
 // Create a pool of database clients to efficiently manage connections
